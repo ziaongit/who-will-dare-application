2fb898f629853febf10fb4f8c8d6c266e7e3484e
Zia <zia_gt@yahoo.com>
2017-07-15 00:27:22 +0200

    Inicial commit

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: '\n \n ',
+             },
+            dist: {
+                src: ['bower_components/jquery/dist/jquery.min.js','bower_components/bootstrap/dist/js/bootstrap.min.js','resources/js/main.js'],
+                dest: 'resources/js/bin/main.js',
+            }
+        },
+        uglify: {
+            options: {
+            mangle:false
+            },
+            my_target: {
+            files: {
+                'resources/js/bin/main.min.js': ['resources/js/bin/main.js']
+            }
+            }
+        }
+    });
+
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    //Tasks
+    grunt.registerTask('CompileJavaScript', ['concat','uglify']);
+    grunt.registerTask('default', ['concat','uglify']);
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# who-will-dare-app
+This is random name generaton applicaiton. User can enter how many names he/she want. The application algorithm will take one random name and display.
+This application will take name from user and display one name randamly for dare are clear from its name.
+
+## Technologies used
+1. HTML
+2. CSS
+3. JavaScript
+4. JSON
