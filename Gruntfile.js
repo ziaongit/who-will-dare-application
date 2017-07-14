a0ee5db7d59ec1cef1e4011dedfb895e0c5d00cd
Zia <zia_gt@yahoo.com>
2017-07-15 01:35:17 +0200

    Folder Structure is ready"

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
         concat: {
             options: {
                 separator: '\n \n ',
@@ -9,6 +10,10 @@ module.exports = function (grunt) {
             dist: {
                 src: ['bower_components/jquery/dist/jquery.min.js','bower_components/bootstrap/dist/js/bootstrap.min.js','resources/js/main.js'],
                 dest: 'resources/js/bin/main.js',
+            },
+            styles: {
+            src: ['bower_components/bootstrap/dist/css/bootstrap.min.css', 'resources/css/main.css'],
+            dest: 'resources/css/bin/main.min.css'
             }
         },
         uglify: {
@@ -26,6 +31,7 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     //Tasks
     grunt.registerTask('CompileJavaScript', ['concat','uglify']);
